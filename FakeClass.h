18d653a807b3a35c1621b099fcca6ad56d2496f5
Renan Martins <heynan0@github.com>
2019-12-30 11:35:56 -0800

    Add todo comments

--- FakeClass.h
+++ FakeClass.h
@@ -1,9 +1,17 @@
 #include <string>
 
+/**
+ * Does this TODO get caught ?
+ */
 class FakeClass
 {
     public:
     std::string getFakeToken();
+    /*
+     * TODO: do something multiline
+     */
 
     private:
+
+    // TODO: something
 };
